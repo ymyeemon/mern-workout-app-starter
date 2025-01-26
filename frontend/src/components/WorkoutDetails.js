27deae4cf40f7278a6229e668d4293bcb4a78f31
ymyeemon <23008873@myrp.edu.sg>
2025-01-26 23:57:43 +0800

    commit

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -8,7 +8,7 @@ const WorkoutDetails = ({ workout }) => {
     const { dispatch } = useWorkoutsContext();
 
     const handleClick = async () => {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/workouts/${workout.id}`, {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/workouts/${workout._id}`, {
             method: 'DELETE'
         });
 
